b6e51e702711fb03dc8575b62fbf9054e8090b02
Nathan Rogers <nathanroger314@gmail.com>
2026-03-15 21:21:58 -0400

    adding cyuuda error checking for operations;

---
+++ src/cuda/utils.cuh
@@ -0,0 +1,14 @@
+#pragma once
+#include <cuda_runtime.h>
+
+
+#define CUDA_CHECK(call)                                                        \
+    do {                                                                        \
+        cudaError_t err = (call);                                               \
+        if (err != cudaSuccess) {                                               \
+            fprintf(stderr, "CUDA error at %s:%d — %s\n",                      \
+                    __FILE__, __LINE__, cudaGetErrorString(err));               \
+            exit(EXIT_FAILURE);                                                 \
+        }                                                                       \
+    } while (0)
+
