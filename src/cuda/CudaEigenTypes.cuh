334b113cb8a5ff8162517a6d4376c8c5f68a448a
Nathan Rogers <nathanroger314@gmail.com>
2026-03-16 11:46:19 -0400

    removing hand rolled types for decomposition in favor of eigen

--- src/cuda/CudaEigenTypes.cuh
+++ src/cuda/CudaEigenTypes.cuh
@@ -22,134 +22,14 @@ using Matrix63dDNA = Eigen::Matrix<double, 6, 3, Eigen::DontAlign>;
 struct Coorespondences {
   Vector3dDNA query;
   Vector3dDNA source;
-};
-
-struct TransposedJacobian{
-  double mat[18];
-
-  double& operator[](const int index){
-    return mat[index];
-  }
-};
-
-struct Jacobian3x6{
-  double mat[18];
-  static constexpr int rows = 3;
-  static constexpr int cols = 6;
-
-  __device__ __inline__ double &operator()(int row, int col) { return mat[row * cols + col]; }
-
-  __device__ __inline__ const double &operator()(int row, int col) const {
-    return mat[row * cols + col];
-  }
-
-  __device__ __inline__ double get(int row, int col) const { return mat[row * cols + col]; }
-
-  // Initialize with zeros
-  __device__ __inline__ void zero() {
-#pragma unroll
-    for (int i = 0; i < 18; i++) {
-      mat[i] = 0.0;
-    }
-  }
-  __device__ __inline__ void setViaHat(double R[9]) {
-    mat[3] = -R[0];
-    mat[4] = -R[1];
-    mat[5] = -R[2];
-    mat[9] = -R[3];
-    mat[10] = -R[4];
-    mat[11] = -R[5];
-    mat[15] = -R[6];
-    mat[16] = -R[7];
-    mat[17] = -R[8];
-  }
-
-  __device__ __inline__ void setIdentity() {
-    mat[0] = 1;
-    mat[7] = 1;
-    mat[14] = 1;
-  }
-
-  __device__ __inline__ void add(Jacobian3x6 other) {
-#pragma unroll
-    for (int i = 0; i < 18; i++) {
-      mat[i] += other.mat[i];
-    }
-  }
-
-  /**
-   * @brief Multiplies the jacobian by the transpose of the other jacobian
-   * transpose this jacobian and multiply by it by other
-   * @param other  the
-   * @param result
-   */
-  __device__ __inline__ TransposedJacobian transpose() {
-
-    // transpose this jacobian
-    TransposedJacobian t;
-#pragma unroll cols
-    for (int i = 0; i < cols; i++) {
-#pragma unroll rows
-      for (int j = 0; j < rows; j++) {
-        t.mat[i * rows + j] = mat[j * cols + i];
-      }
-    }
-    return t;
-  }
-
-  __device__ __inline__ void multiplyScalar(double scalar) {
-#pragma unroll 18
-    for (int i = 0; i < 18; i++) {
-      mat[i] *= scalar;
-    }
-  }
-
-  __device__ void operator* (double scalar){
-    multiplyScalar(scalar);
-  }
+  bool actualized;''
 };
 
 
-struct HessianAndVector {
-  double mat[36];
-  double vec[6];
-  static constexpr int rows = 6;
-  static constexpr int cols = 6;
-
-  __device__ __forceinline__ double &operator()(int row, int col) { return mat[row * cols + col]; }
-
-  __device__ __forceinline__ void vecSet(int index, double value) { vec[index] = value; }
-
-  __device__ __forceinline__ void zero() {
-    #pragma unroll 36
-    for (int i = 0; i < 36; i++) {
-      mat[i] = 0.0;
-    }
-  }
-
-  __device__ __inline__ void
-  computeFromJacobianResidual(Jacobian3x6 jacobian, Vector3dDNA residual, double weight) {
-    TransposedJacobian transposed_jacobian_mat = jacobian.transpose();
-    jacobian.multiplyScalar(weight);
-#pragma unroll 
-    for (int i = 0; i < 6; i++) {
-#pragma unroll 
-      for (int j = 0; j < 6; j++) {
-#pragma unroll 
-        for (int k = 0; k < 3; k++) {
-          mat[i * cols + j] += transposed_jacobian_mat[i * jacobian.rows + k] * jacobian(k, j);
-        }
-      }
-    }
-
-  }
-};
-
-
-struct jacobian_residual {
-  Jacobian3x6 jacobian;
-  Vector3dDNA residual;
-};
+struct LinearSystem{
+  Matrix6dDNA A;
+  Vector6dDNA B;
+}
 
 
 using LinearSystem = HessianAndVector;
